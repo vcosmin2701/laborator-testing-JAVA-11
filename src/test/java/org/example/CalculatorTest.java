7be5c5f6c5a10b7b631b67f810e67322955b559b
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-22 11:08:37 +0300

    TestConstructor Unit Test

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -6,30 +6,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     @Test
-    void multiply() {
-        // arrange
+    void TestMultiply() {
         Calculator calc = new Calculator(4);
-        // act
         calc.multiply(2);
-        // assert
         assertEquals(8, calc.getResult());
     }
 
     @Test
-    void addition() {
+    void TestAddition() {
         Calculator calc = new Calculator(4);
         calc.addition(1);
         assertEquals(5, calc.getResult());
     }
 
     @Test
-    void division() {
+    void TestDivision() {
         Calculator calc = new Calculator(4);
         float actualResult = calc.division(2).getResult();
-        assertEquals(2, actualResult, "4/2 did not produce 2");
+        assertEquals(2, actualResult);
     }
 
     @Test
-    void squareRoot() {
+    void TestSquareRoot() {
+        Calculator calc = new Calculator(4);
+        float actualResult = calc.squareRoot().getResult();
+        assertEquals(4, actualResult);
     }
+
+    @Test
+    public void TestConstructorAndGetResult() {
+        Calculator calculator = new Calculator(10);
+        float result = calculator.getResult();
+        assertEquals(10, result);
+    }
+
+
+
 }
